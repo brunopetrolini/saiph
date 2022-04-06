c69bbcebd19573807daaf8127deb164d4088a413
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-04-06 14:35:17 -0300

    add list all students

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
-import { StudentsService } from '../../../services/students.service';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
 
+import { StudentsService } from '../../../services/students.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { Student } from '../models/student.model';
 
 @Resolver(() => Student)
 export class StudentsResolver {
   constructor(private readonly studentsService: StudentsService) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  async students() {
+    return this.studentsService.listAllStudents();
+  }
 }
--- classroom/src/services/students.service.ts
+++ classroom/src/services/students.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+
 @Injectable()
-export class StudentsService {}
+export class StudentsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async listAllStudents() {
+    return await this.prisma.students.findMany();
+  }
+}
