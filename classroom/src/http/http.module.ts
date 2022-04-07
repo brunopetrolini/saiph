d92df20c490e4be3806fcdca19f8bb130d0c7dcc
Bruno Petrolini <brunopetrolini@hotmail.com>
2022-04-07 20:35:46 -0300

    add apollo federation gateway

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -5,7 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { DatabaseModule } from '../database/database.module';
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { CoursesResolver } from './graphql/resolvers/courses.resolver';
 import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
 import { StudentsResolver } from './graphql/resolvers/students.resolver';
@@ -17,8 +20,8 @@ import { StudentsService } from '../services/students.service';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
